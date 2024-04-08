ed20e1a74ccf759b4d2bbc0d5826556542bb8813
Ian Reimers <ian.reimers.apps@gmail.com>
2024-04-07 23:53:15 -0600

    fix: allow only /register and /login endpoints with no auth

--- server/src/main/java/org/opensourcecommunity/pomodoroapp/config/SecurityConfiguration.java
+++ server/src/main/java/org/opensourcecommunity/pomodoroapp/config/SecurityConfiguration.java
@@ -1,14 +1,13 @@
 package org.opensourcecommunity.pomodoroapp.config;
 
 import java.util.Arrays;
-
+import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.CorsConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
@@ -16,48 +15,44 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
-import lombok.RequiredArgsConstructor;
-
 @Configuration
 @EnableWebSecurity
 @RequiredArgsConstructor
 public class SecurityConfiguration {
 
-	private final JwtAuthenticationFilter jwtAuthFilter;
-	private final AuthenticationProvider authenticationProvider;
+  private final JwtAuthenticationFilter jwtAuthFilter;
+  private final AuthenticationProvider authenticationProvider;
 
-	@Bean
-	public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
-		httpSecurity
-				.cors(Customizer.withDefaults())
-				.csrf(csrf -> csrf.disable())
-				.authorizeHttpRequests(authorize -> {
-					authorize.requestMatchers("/api/**").permitAll();
-					authorize.anyRequest().authenticated();
+  @Bean
+  public SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity
+        .cors(Customizer.withDefaults())
+        .csrf(csrf -> csrf.disable())
+        .authorizeHttpRequests(
+            authorize -> {
+              authorize.requestMatchers("/api/v1/register", "/api/v1/login").permitAll();
+              authorize.anyRequest().authenticated();
+            })
+        .sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+        .authenticationProvider(authenticationProvider)
+        .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
-				})
-				.sessionManagement(sess -> sess.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-				.authenticationProvider(authenticationProvider)
-				.addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
+    return httpSecurity.build();
+  }
 
-		return httpSecurity
-				.build();
-	}
-
-	@Bean
-	public CorsConfigurationSource corsConfigurationSource() {
-		CorsConfiguration configuration = new CorsConfiguration();
-		configuration.setAllowedOrigins(Arrays.asList("*"));
-		configuration.setAllowedMethods(Arrays.asList("*"));
-		configuration.setAllowedHeaders(Arrays.asList("*"));
-		// configuration.setAllowedOrigins(Arrays.asList("*"));
-		// configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH",
-		// "DELETE"));
-		// configuration.setAllowedHeaders(Arrays.asList("Authorization",
-		// "Content-Type"));
-		UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-		source.registerCorsConfiguration("/**", configuration);
-		return source;
-	}
-
+  @Bean
+  public CorsConfigurationSource corsConfigurationSource() {
+    CorsConfiguration configuration = new CorsConfiguration();
+    configuration.setAllowedOrigins(Arrays.asList("*"));
+    configuration.setAllowedMethods(Arrays.asList("*"));
+    configuration.setAllowedHeaders(Arrays.asList("*"));
+    // configuration.setAllowedOrigins(Arrays.asList("*"));
+    // configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PATCH",
+    // "DELETE"));
+    // configuration.setAllowedHeaders(Arrays.asList("Authorization",
+    // "Content-Type"));
+    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+    source.registerCorsConfiguration("/**", configuration);
+    return source;
+  }
 }

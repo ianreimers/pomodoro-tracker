4ee54e244a8384e6e9dd0241854560957726f436
Ian Reimers <ian.reimers.apps@gmail.com>
2024-04-08 00:09:52 -0600

    chore: add user and user settings dto creation methods

--- server/src/test/java/org/opensourcecommunity/pomodoroapp/util/TestDataFactory.java
+++ server/src/test/java/org/opensourcecommunity/pomodoroapp/util/TestDataFactory.java
@@ -1,12 +1,18 @@
 package org.opensourcecommunity.pomodoroapp.util;
 
+import java.time.ZonedDateTime;
 import java.util.UUID;
 import org.opensourcecommunity.pomodoroapp.dtos.PomodoroSessionDto;
+import org.opensourcecommunity.pomodoroapp.dtos.PomodoroSessionPatchDto;
 import org.opensourcecommunity.pomodoroapp.dtos.PomodoroSessionResponseDto;
+import org.opensourcecommunity.pomodoroapp.dtos.UserDto;
+import org.opensourcecommunity.pomodoroapp.dtos.UserResponseDto;
+import org.opensourcecommunity.pomodoroapp.dtos.UserSettingsDto;
 import org.opensourcecommunity.pomodoroapp.models.BreakTypeEnum;
 import org.opensourcecommunity.pomodoroapp.models.PomodoroSession;
 import org.opensourcecommunity.pomodoroapp.models.Role;
 import org.opensourcecommunity.pomodoroapp.models.User;
+import org.opensourcecommunity.pomodoroapp.models.UserSettings;
 
 public class TestDataFactory {
 
@@ -22,6 +28,51 @@ public class TestDataFactory {
         .password("password")
         .role(Role.USER)
         .build();
+  }
+
+  public static UserDto createUserDto() {
+    User user = createUser();
+    return new UserDto(user.getUsername(), user.getEmail(), user.getPassword());
+  }
+
+  public static UserDto createUserDto(User user) {
+    return new UserDto(user.getUsername(), user.getEmail(), user.getPassword());
+  }
+
+  public static UserResponseDto createUserResponseDto() {
+    User user = createUser();
+    UserSettings userSettings = createUserSettings(1L, user);
+    return new UserResponseDto(1L, "user", "user@example.com", userSettings);
+  }
+
+  public static UserResponseDto createUserResponseDto(User user) {
+    UserSettings userSettings = createUserSettings(1L, user);
+    return new UserResponseDto(1L, "user", "user@example.com", userSettings);
+  }
+
+  public static UserResponseDto createUserResponseDto(User user, UserSettings userSettings) {
+    return new UserResponseDto(1L, "user", "user@example.com", userSettings);
+  }
+
+  public static UserSettings createUserSettings(Long userSettingsId, User user) {
+    return UserSettings.builder()
+        .id(userSettingsId)
+        .taskSeconds(1500)
+        .shortBreakSeconds(600)
+        .longBreakSeconds(1800)
+        .pomodoroInterval(4)
+        .sound("bells")
+        .user(user)
+        .build();
+  }
+
+  public static UserSettingsDto createUserSettingsDto(UserSettings userSettings) {
+    return new UserSettingsDto(
+        userSettings.getTaskSeconds(),
+        userSettings.getShortBreakSeconds(),
+        userSettings.getLongBreakSeconds(),
+        userSettings.getPomodoroInterval(),
+        userSettings.getSound());
   }
 
   public static PomodoroSession createPomodoroSession() {
@@ -43,12 +94,18 @@ public class TestDataFactory {
         .tempUuid(UUID.randomUUID())
         .taskDuration(0)
         .sessionTaskSeconds(1500)
+        .sessionStartTime(ZonedDateTime.now())
+        .sessionUpdateTime(ZonedDateTime.now())
         .sessionShortBreakSeconds(600)
         .sessionLongBreakSeconds(1900)
         .breakDuration(0)
         .breakType(BreakTypeEnum.SHORT)
         .user(user)
         .build();
+  }
+
+  public static PomodoroSessionPatchDto createPomodoroSessionPatchDto(String sessionType) {
+    return new PomodoroSessionPatchDto(sessionType);
   }
 
   public static PomodoroSessionResponseDto createPomodoroSessionResponseDto(
